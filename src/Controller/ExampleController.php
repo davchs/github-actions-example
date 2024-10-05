b8f808258d1e9dd45578ceb26a51569542315e2a
David Chalupsky <davchs@pm.me>
2024-10-05 09:03:31 +0200

    setup symfony project

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ExampleController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
